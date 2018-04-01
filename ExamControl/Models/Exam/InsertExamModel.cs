e792913ec33c903e61a4e4cff748c6d719492002
Arie Schouten <arie-schouten@outlook.com>
2018-04-01 15:28:41 +0200

    Arie is baas

---
+++ ExamControl/Models/Exam/InsertExamModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExamControl.Models.Exam
+{
+    public class InsertExamModel
+    {
+        public bool ClassroomHasComputers { get; set; }
+
+        [Required]
+        public uint EstimatedAmountOfStudents { get; set; }
+
+        [Required]
+        public HttpPostedFileBase File { get; set; }
+
+        public bool ExamRegistrationSurveillantAvailable { get; set; }
+
+        [Required]
+        public int ExamSubject { get; set; }
+    }
+}
