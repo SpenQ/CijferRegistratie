47e7dc6657481418becbcadcf3f8b65be99ed9f4
Freek <Freek@WINDELL-NQ9GBDL>
2018-04-02 13:38:37 +0200

    RegisterExamModel added

---
+++ ExamControl/Models/Exam/RegisterExamModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExamControl.Models.Exam
+{
+    public class RegisterExamModel
+    {
+        [Required]
+        public DateTime ExamDateTime { get; set; }
+
+        [Required]
+        public TimeSpan ExamDuration { get; set; }
+
+        public string ClassroomCode { get; set; }
+
+        [Required]
+        public int ExamSubject { get; set; }
+    }
+}
